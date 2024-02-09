c83eec1667eb21127798e653195719f9baa540f9
Your Name <you@example.com>
2024-02-10 00:38:03 +0100

    you

--- models/base_model.py
+++ models/base_model.py
@@ -7,12 +7,22 @@ from datetime import datetime
 class BaseModel:
     """ BaseModel """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Init Method"""
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    if k == 'id':
+                        v = str(v)
+                    elif k in ['created_at', 'updated_at']:
+                        v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ Str """
