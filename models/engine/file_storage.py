13e7557e457840e6cfd2f4475e7510acdcf1d78c
under20techie <133390683+under20techie@users.noreply.github.com>
2024-02-12 02:34:07 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,7 +18,7 @@ class FileStorage:
 
     def new(self, obj):
         """ sets the obj with key <obj class name>.id"""
-        key = f"{obj.__class__}.{obj.id}"
+        key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
@@ -34,7 +34,7 @@ for k, v in self.__objects.items()}
             with open(self.__file_path, 'r') as f:
                 new_object = json.load(f)
                 for k, v in new_object.items():
-                    func = f"{v['__class__']}({**v})"
+                    func = f"{v['__class__']}(**v)"
                     self.__objects[k] = eval(func)
         except Exception as e:
             pass
