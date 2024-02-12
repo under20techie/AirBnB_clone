f4ff902a54b15ea25b21858f658d5ffacc60bbc6
under20techie <133390683+under20techie@users.noreply.github.com>
2024-02-12 02:49:31 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -30,7 +30,7 @@ class BaseModel:
     def __str__(self):
         """ Str """
 
-        return "[{}] ({}) <{}>".format(self.__class__.__name__
+        return "[{}] ({}) {}".format(self.__class__.__name__
 , self.id, self.__dict__)
 
     def save(self):
