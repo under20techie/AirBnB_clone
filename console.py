1e226a4f35365c59631044083510424a3c957964
under20techie <133390683+under20techie@users.noreply.github.com>
2024-02-14 00:47:16 +0100

    Update console.py

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Console """
 import cmd
+import models as md
 from models.base_model import BaseModel
 
 
@@ -20,7 +21,112 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Empty"""
         return
+def do_create(self, cls_name):
+        """Creates a new instance of BaseModel, save it to json file """
+        if cls_name:
+            if not isinstance(cls_name, 'BaseModel'):
+                print("** class doesn't exist **")
+                return
 
+            new_instance = eval(cls_name)()
+            new_instance.save()
+            print(new_instance.id)
+        else:
+            print("** class name missing **")
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance"""
+    
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        cls_name = args[0]
+
+        if not isinstance(cls_name, 'BaseModel'):
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        id_no = args[1]
+
+        try:
+            obj = md.storage.all()[f"{cls_name}.{id_no}"]
+            print(obj)
+        except KeyError:
+            print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id """
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+
+        cls_name = args[0]
+        if not isinstance(cls_name, 'BaseModel'):
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        id_no = args[1]
+        try:
+            obj = md.storage.all()
+            del obj[f"{cls_name}.{id_no}"]
+            md.storage.save()
+        except KeyError:
+            print("** no instance found **")
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances """
+    
+        if arg:
+            if not isinstance(arg, 'BaseModel'):
+                print("** class doesn't exist **")
+                return
+            print([str(obj) for obj in md.storage.all().values()])
+
+    def do_update(self, arg):
+        """Updates an instance"""
+
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+
+        cls_name = args[0]
+
+        if not isinstance(cls_name, 'BaseModel'):
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        id_no = args[1]
+
+        try:
+            obj = md.storage.all()[f"{cls_name}.{reg_no}"]
+        except KeyError:
+            print("** no instance found **")
+            return
+
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+
+        if len(args) < 4:
+            print("** value missing **")
+            return
+
+        setattr(obj, args[2], args[3])
+        md.storage.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
