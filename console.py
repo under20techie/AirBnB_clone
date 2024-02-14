c1d425345fd6c9b92e462512eb895481a61ac720
Your Name <you@example.com>
2024-02-14 12:07:32 +0100

    you

--- console.py
+++ console.py
@@ -4,6 +4,12 @@ import cmd
 from inspect import isclass
 import models as md
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.amenity import Amenity
+from models.city import City
+from models.state import State
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
