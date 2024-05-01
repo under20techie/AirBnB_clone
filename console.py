e8b84abff07585fe53149053805be85bb1883bcb
under20techie <133390683+under20techie@users.noreply.github.com>
2024-05-01 01:03:57 +0100

    Update console.py

--- console.py
+++ console.py
@@ -174,9 +174,9 @@ class HBNBCommand(cmd.Cmd):
 
         par_2 = str(par_1[1])
 
-        if func == "update" and len(par_2) == 2:
+        if func == "update" and len(par_1) == 2:
             par_2 = eval('{' + par_1[1] + ', ' + par_1[2])
-            while k, v in par_2.items():
+            for k, v in par_2.items():
                 par = "{} {} {} {}".format(cls_name, par_1[0], k, v)
                 eval(f"self.do_{func}")(par)
             return
